30ca5eb911b546708ed363cd3b0941e4b128bd9a
alexss3 <stultzat@gmail.com>
2020-07-09 17:05:42 +0200

    Adding CustomError abstract class and simplified error handling. Added basic framework for connecting to mongodb container and service

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,5 +1,7 @@
 import express from 'express';
+import 'express-async-errors';
 import { json } from 'body-parser';
+import mongoose from 'mongoose';
 
 import currentUserRouter from './routes/current-user';
 import signinRouter from './routes/signin';
@@ -7,6 +9,7 @@ import signoutRouter from './routes/signout';
 import signupRouter from './routes/signup';
 
 import { errorHandler } from './middlewares/error-handler';
+import { NotFoundError } from './errors/not-found-error';
 
 const app = express();
 app.use(json());
@@ -16,8 +19,28 @@ app.use(signinRouter);
 app.use(signoutRouter);
 app.use(signupRouter);
 
+app.all('*', () => {
+    throw new NotFoundError();
+});
+
 app.use(errorHandler);
 
-app.listen(3000, () => {
-    console.log('Listening on port 3000!!!');
-});
+const start = async () => {
+    try {
+        await mongoose.connect('mongodb://auth-mongo-srv:27017/auth', {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useCreateIndex: true
+        });
+        console.log('Connected to MongoDB');
+        
+    } catch (error) {
+        console.error(error);
+    }
+
+    app.listen(3000, () => {
+        console.log('Listening on port 3000!!!');
+    });
+};
+
+start();
